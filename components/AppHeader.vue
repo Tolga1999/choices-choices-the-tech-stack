0e36e322fa8570adcf33d03c65346f9c83d89274
Tolga1999 <tolgadoganer@outlook.com>
2023-10-19 18:30:15 +0200

    completed global css + header styling

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,15 +1,43 @@
 <template>
     <header>
         <nav>
-            <li>Home</li>
-            <li>Partner</li>
-            <li>Account</li>
+            <a href="/">
+                <img src=/img/vervoerregio_amsterdam_logo.svg alt="logo vervoerregio" />
+            </a>
+
+            <section class="header-icons">
+                <a href="/info">
+                    <img class="information-icon-img" src=/img/information_icon.svg alt="information icon" />
+                </a>
+                <img src=/img/dark_mode_icon.svg alt="darkmode icon" />
+            </section>
         </nav>
     </header>
 </template>
 
 <style>
-header{
-    background-color: red;
+header {
+    position: sticky;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    background-color: #202020;
+    padding: 1em;
+    border-bottom: 2px solid #454545;
+}
+
+.header-icons {
+    display: flex;
+    gap: 1em;
+    align-items: center;
+}
+
+.information-icon-img {
+    display: block;
 }
 </style>
