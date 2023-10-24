c5df88ef7dc2c348b6b217d377bfca83a48812f5
Tolga1999 <tolgadoganer@outlook.com>
2023-10-24 21:54:04 +0200

    dynamic image

---
+++ components/Partner.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>tolga</h2>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,9 @@ const QUERY = `
   allPartners {
     id
     title
+    logo {
+      url
+    }
   }
 }
 `;
@@ -16,8 +19,15 @@ const { data, error } = await useGraphqlQuery({ query: QUERY });
 
 <template>
   <main>
-    {{ data }}
-    <h1>Hello</h1>
+    <h1>Partners</h1>
+
+    <Partner />
+    
+    <section v-for="partner in data.allPartners">
+      <h2>{{ partner.title }}</h2>
+      <img :src="partner.logo.url" alt="partner logo">
+    </section>
+    
     <NuxtLink to="partner">Partner link</NuxtLink>
   </main>
 </template>
